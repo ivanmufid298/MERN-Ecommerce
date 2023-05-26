629c429fc8bd9d30faa31c8b1a9a61b26552063f
ivan mufid <ivanmohammad2355@gmail.com>
2023-05-26 11:31:46 +0700

    login token

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { registerController } from "../controllers/authController.js";
-
+import {
+  registerController,
+  loginController,
+} from "../controllers/authController.js";
 //router object
 const router = express.Router();
 
 //routing
 //register || method POST
 router.post("/register", registerController);
+
+//login || POST
+router.post("/login", loginController);
 
 export default router;
